fef0c337f98f10c43a627458964c7f54a5a812b7
masiyematome <matomemasiye7@gmail.com>
2021-03-27 21:08:37 +0200

    Completed the index page

--- toggleButton.js
+++ toggleButton.js
@@ -1,8 +1,10 @@
 const toggleButton = document.querySelector(".toggle-button");
 const navbarLinks = document.querySelector(".navbar-links");
+const mainMenu = document.querySelector(".mainMenu");
 
 toggleButton.addEventListener("click" , function(){
     navbarLinks.classList.toggle("on-off");
     this.classList.toggle("on-off");
+    mainMenu.classList.toggle("on-off");
 })
 
